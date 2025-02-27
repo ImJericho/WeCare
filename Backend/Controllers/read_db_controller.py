767f576c2ae8f99ba80e88442ded19e9a55d1952
Pranay Pandey <pranaypandeyofficial@gmail.com>
2025-02-27 18:08:08 +0530

    Add endpoint to retrieve doctor details for a specific patient

--- Backend/Controllers/read_db_controller.py
+++ Backend/Controllers/read_db_controller.py
@@ -70,6 +70,39 @@ def get_doctor_detail(doctor_id):
                 mimetype='application/json'
             )
 
+@viewer_db.route('/get_my_doctor_detail/<patient_id>', methods = ["GET"])
+def get_doctor_of_patient(patient_id):
+    try:
+        db = current_app.patient_doctor_db
+        if not db.if_patient_exists(patient_id):
+            return Response(
+                response=json.dumps({'status': "failed",
+                                        'message': "Patient with this doctor_id does not exists"}),
+                status=400,
+                mimetype='application/json'
+            )
+        res, msg = db.get_doctor_of_patient(patient_id)
+        if res:
+            return get_doctor_detail(res[0])
+        else:
+            return Response(
+                response=json.dumps({
+                    'status': "failed",
+                    'message': msg
+                }),
+                status=404,
+                mimetype='application/json'
+            )
+    except Exception as e:
+        return Response(
+                response=json.dumps({'status': "failed", 
+                                     "message": "Error Occured",
+                                     "error": str(e)}),
+                status=500,
+                mimetype='application/json'
+            )
+
+
 @viewer_db.route('/get_patient_detail/<patient_id>', methods = ["GET"])
 def get_patient_detail(patient_id):
     try:
